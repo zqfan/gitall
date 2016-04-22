4ec4ba518db108cb03687ec66af5a4a3611995df
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-04-23 00:55:53 +0800

    add console command entry points and requirements

--- gitall/gitall.py
+++ gitall/gitall.py
@@ -5,32 +5,36 @@ import os
 import git
 import requests
 
-repo_html_urls = {}
-repo_page_count = 1
-next_url = 'https://api.github.com/orgs/openstack/repos'
-while next_url:
-    print 'request repo page %s' % repo_page_count
-    resp = requests.get(next_url)
-    if resp.status_code == 403:
-        print 'API rate limit exceeded, exit'
-        exit(1)
-    repo_page = json.loads(resp.content)
-    for repo in repo_page:
-        repo_html_urls[repo['name']] = repo['html_url']
-    next_url = resp.links.get('next', {}).get('url')
-    repo_page_count += 1
+def main():
+    repo_html_urls = {}
+    repo_page_count = 1
+    next_url = 'https://api.github.com/orgs/openstack/repos'
+    while next_url:
+        print 'request repo page %s' % repo_page_count
+        resp = requests.get(next_url)
+        if resp.status_code == 403:
+            print 'API rate limit exceeded, exit'
+            exit(1)
+        repo_page = json.loads(resp.content)
+        for repo in repo_page:
+            repo_html_urls[repo['name']] = repo['html_url']
+        next_url = resp.links.get('next', {}).get('url')
+        repo_page_count += 1
 
-print 'total repo count: %s' % len(repo_html_urls)
+    print 'total repo count: %s' % len(repo_html_urls)
 
-repo_count = 1
-for name, url in repo_html_urls.items():
-    dest_dir = os.path.join('./', name)
-    if not os.path.exists(dest_dir):
-        print '%s: %s: git clone %s' % (repo_count, name, url)
-        repo = git.Repo.clone_from(url, dest_dir)
-    else:
-        print '%s: %s: git pull --ff-only' % (repo_count, name)
-        git.cmd.Git(dest_dir).pull(ff_only=True)
-    repo_count += 1
+    repo_count = 1
+    for name, url in repo_html_urls.items():
+        dest_dir = os.path.join('./', name)
+        if not os.path.exists(dest_dir):
+            print '%s: %s: git clone %s' % (repo_count, name, url)
+            repo = git.Repo.clone_from(url, dest_dir)
+        else:
+            print '%s: %s: git pull --ff-only' % (repo_count, name)
+            git.cmd.Git(dest_dir).pull(ff_only=True)
+        repo_count += 1
 
-print 'all %s repositories succeed!' % repo_count - 1
+    print 'all %s repositories succeed!' % repo_count - 1
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'gitall',
-    packages = ['gitall'], # this must be the same as the name above
-    version = '0.1.1',
+    packages = find_packages(),
+    version = '0.1.2',
+    install_requires = [
+        'GitPython',
+        'requests'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'gitall = gitall.gitall:main',
+        ],
+    },
     description = 'clone/pull all repositories of a specified orgnization',
     author = 'ZhiQiang Fan',
     author_email = 'zqfan.dev+gitall@gmail.com',
     url = 'https://github.com/zqfan/gitall',
-    download_url = 'https://github.com/zqfan/gitall/tarball/0.1.1',
+    download_url = 'https://github.com/zqfan/gitall/tarball/0.1.2',
     keywords = ['github', 'gitall'],
     classifiers = [],
 )
