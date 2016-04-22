da0bb4853c5b2fe1d195fe788d61c4f5781d9644
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-04-23 00:05:55 +0800

    first release for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'gitall',
+    packages = ['gitall'], # this must be the same as the name above
+    version = '0.1.0',
+    description = 'clone/pull all repositories of a specified orgnization',
+    author = 'ZhiQiang Fan',
+    author_email = 'zqfan.dev+gitall@gmail.com',
+    url = 'https://github.com/zqfan/gitall',
+    download_url = 'https://github.com/zqfan/gitall/tarball/0.1.0',
+    keywords = ['github', 'gitall'],
+    classifiers = [],
+)
