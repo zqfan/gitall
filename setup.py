34bc88864f1fa32a037a1e7ed89cee6c4b5c9642
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-04-23 00:14:25 +0800

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'gitall',
     packages = ['gitall'], # this must be the same as the name above
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'clone/pull all repositories of a specified orgnization',
     author = 'ZhiQiang Fan',
     author_email = 'zqfan.dev+gitall@gmail.com',
     url = 'https://github.com/zqfan/gitall',
-    download_url = 'https://github.com/zqfan/gitall/tarball/0.1.0',
+    download_url = 'https://github.com/zqfan/gitall/tarball/0.1.1',
     keywords = ['github', 'gitall'],
     classifiers = [],
 )
